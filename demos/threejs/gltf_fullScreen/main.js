8cb0d55e3173ecc8e3aae40796e1070050d558e7
OzakiSatoshi <60631984+OzakiSatoshi@users.noreply.github.com>
2022-12-04 16:52:12 +0900

    usamimi up

--- demos/threejs/gltf_fullScreen/main.js
+++ demos/threejs/gltf_fullScreen/main.js
@@ -1,6 +1,6 @@
 // SETTINGS of this demo:
 const SETTINGS = {
-  gltfModelURL: 'DamagedHelmet/glTF/DamagedHelmet.gltf',
+  gltfModelURL: 'models/gltf/usamimi.gltf',
   cubeMapURL: 'Bridge2/',
   offsetYZ: [0.3, 0], // offset of the model in 3D along vertical and depth axis
   scale: 2.5
